3e891d97e1b34f91ddc7a41cfe32cea4377756c6
Aleksand.mzg <aleksandr.mzg@yandex.ru>
2023-09-20 11:53:08 +0500

    распознает без учета регистра

--- examples/example005_lubimchik/Program.cs
+++ examples/example005_lubimchik/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите имя пользователя");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "Саша"
+)
+{
+    Console.WriteLine("Ура это же Саша!");
+}
+else
+{
+Console.Write("Привет!,");
+Console.Write(username);
+}
