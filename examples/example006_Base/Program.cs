b87a5b4fae20daedd32d733faf2d98a6191c30b6
Aleksand.mzg <aleksandr.mzg@yandex.ru>
2023-09-20 12:04:04 +0500

    программа с гирями

--- examples/example006_Base/Program.cs
+++ examples/example006_Base/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int a = 1;
+int b = 25;
+int c = 4;
+int d = 8;
+int e = 3;
+
+
+int max = a;
+
+if (a > max ) max = a;
+if (b > max ) max = b;
+if (c > max ) max = c;
+if (d > max ) max = d;
+if (e > max ) max = e;
+
+Console.WriteLine(max);
